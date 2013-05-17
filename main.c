4f66f8287e35ad595d072f189f6b4fed35a23d21
jichaow <wangqingcaoai@sina.com>
2013-05-17 22:55:40 +0800

    change

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "transfar/server.h"
-#include "util/config.h"
-#include "base_queue/topic_test.c"
-int main(int argc, char const *argv[])
-{
-    //initConfig();
-    return 0;
-}
